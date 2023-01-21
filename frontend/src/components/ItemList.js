7b70166eb6a519f341563404abbe0f8f0c78d0b1
Aamir shaikh <56036364+aamirxshaikh@users.noreply.github.com>
2023-01-21 08:38:18 +0000

    Display message for no items found

--- frontend/src/components/ItemList.js
+++ frontend/src/components/ItemList.js
@@ -7,6 +7,14 @@ const ItemList = (props) => {
     return <div className="py-4">Loading...</div>;
   }
 
+  if (props.noResults) {
+    return (
+      <div className="py-4" id="empty">
+        No items found!
+      </div>
+    );
+  }
+
   if (props.items.length === 0) {
     return <div className="py-4 no-items">No items are here... yet.</div>;
   }
