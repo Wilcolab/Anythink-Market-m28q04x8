cddd16a4b9bec43de89cfc484def5adbe6c4e2e2
Aamir shaikh <56036364+aamirxshaikh@users.noreply.github.com>
2023-01-23 19:06:51 +0000

    Update seeds.js

--- backend/scripts/seeds.js
+++ backend/scripts/seeds.js
@@ -1,92 +1,67 @@
 //TODO: seeds script should come here, so we'll be able to put some data in our local env
 
 require("dotenv").config();
+const mongoose = require("mongoose");
 require("../models/User");
 require("../models/Item");
 require("../models/Comment");
-
-const mongoose = require("mongoose");
-const { faker } = require("@faker-js/faker");
-
-if (!process.env.MONGODB_URI) {
-  console.warn("Missing Mongo db URI in .env");
-}
-
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false,
-  useCreateIndex: true,
-});
-
-if (process.env.NODE_ENV !== "production") {
-  mongoose.set("debug", true);
-}
 
 const User = mongoose.model("User");
 const Item = mongoose.model("Item");
 const Comment = mongoose.model("Comment");
 
-const createUsers = async (numUsers) => {
-  const users = [];
-  for (let i = 0; i < numUsers; i++) {
-    users.push({
-      username: faker.database.mongodbObjectId(),
-      email: faker.internet.email(),
-      bio: faker.lorem.paragraph(),
-      image: faker.image.avatar(),
-      role: "user",
-      favorites: [],
-      following: [],
+const connect = () => {
+  const connection = process.env.MONGODB_URI || "mongodb://localhost:27017";
+  mongoose.connect(connection, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  mongoose.set("debug", true);
+};
+
+connect();
+
+async function main() {
+  await User.remove({});
+  await Item.remove({});
+  await Comment.remove({});
+  for (let i = 0; i < 100; i++) {
+    const user = new User();
+    user.username = `user${i}`;
+    user.email = `user${i}@test.com`;
+    await user.save();
+
+    const item = new Item({
+      slug: `slug${i}`,
+      title: `title ${i}`,
+      description: `description ${i}`,
+      seller: user,
     });
+    await item.save();
+
+    let commentIds = [];
+    for (let j = 0; j < 100; j++) {
+      const comment = new Comment({
+        body: `body ${j}`,
+        seller: user,
+        item: item,
+      });
+      await comment.save();
+      commentIds.push(comment._id);
+    }
+    item.comments = commentIds;
+    await item.save();
   }
-  await User.create(users);
-};
+}
 
-const createItems = async (numItems) => {
-  const items = [];
-  for (let i = 0; i < numItems; i++) {
-    const title = faker.commerce.productName() + " " + faker.datatype.uuid();
-    items.push({
-      title,
-      slug: faker.helpers.slugify(title),
-      description: faker.commerce.productDescription(),
-      image: faker.image.image(),
-      tagList: title.split(" "),
-      comments: [],
-    });
-  }
-  await Item.create(items);
-};
-
-const createComments = async (numComments) => {
-  const comments = [];
-  for (let i = 0; i < numComments; i++) {
-    comments.push({
-      body: faker.lorem.paragraph(),
-      seller: await User.findOne()._id,
-      item: await Item.findOne()._id,
-    });
-  }
-  await Comment.create(comments);
-};
-
-const seed = async () => {
-  await User.deleteMany({});
-  await Item.deleteMany({});
-  await Comment.deleteMany({});
-  await createUsers(100);
-  await createItems(100);
-  await createComments(100);
-  console.log("Database seeded successfully!");
-  mongoose.connection.close();
-};
-
-seed()
+main()
   .then(() => {
-    process.exit();
+    console.log("DB seeding complete");
+    mongoose.connection.close();
+    process.exit(0);
   })
-  .catch((e) => {
-    console.error(e);
-    process.exit();
+  .catch((err) => {
+    console.log(`Error ${err.message}`);
+    mongoose.connection.close();
+    process.exit(1);
   });
