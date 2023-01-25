a8b3e1060a3ffe127b9cba74354e45c2eb8a1b59
Aamir shaikh <56036364+aamirxshaikh@users.noreply.github.com>
2023-01-25 18:31:04 +0000

    Refactor Banner.js to add dark theme

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -1,18 +1,32 @@
 import React from "react";
 import logo from "../../imgs/logo.png";
+import styled from 'styled-components';
+
+const BannerWrapper = styled.div`
+    background-color: ${props => props.theme.dark};
+    .container{
+        padding: 1rem;
+        text-align: center;
+    }
+    img{
+        max-width: 100%;
+    }
+`;
 
 const Banner = () => {
   return (
-    <div className="banner text-white">
-      <div className="container p-4 text-center">
-        <img src={logo} alt="banner" />
-        <div>
-          <span>A place to </span>
-          <span id="get-part">get</span>
-          <span> the cool stuff.</span>
+    <BannerWrapper>
+      <div className="banner text-white">
+        <div className="container p-4 text-center">
+          <img src={logo} alt="banner" />
+          <div>
+            <span>A place to </span>
+            <span id="get-part">get</span>
+            <span> the cool stuff.</span>
+          </div>
         </div>
       </div>
-    </div>
+    </BannerWrapper>
   );
 };
 
