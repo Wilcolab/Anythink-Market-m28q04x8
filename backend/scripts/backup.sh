8e65dcc3be68bc633e3e02d5bd6cabe7210f09d5
Aamir shaikh <56036364+aamirxshaikh@users.noreply.github.com>
2023-02-09 08:24:15 +0000

    Add backup.sh

---
+++ backend/scripts/backup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]; then
+    echo "Please provide a file name as an argument."
+    exit 1
+fi
+
+file=$1
+
+if [ ! -f "$file" ]; then
+    echo "File not found."
+    exit 1
+fi
+
+tar -czvf "/backup.tar.gz" "$file"
+
+echo "Archiving complete: /backup.tar.gz"
